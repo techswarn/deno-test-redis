3f7ebbcef7468f576240cf2f86968f4c5f8e1baf
techswarn <tech.swarns@gmail.com>
2025-02-05 12:58:02 +0530

    code update

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ console.log(`Redis URL: ${redisUrl}`);
 console.log(`Redis Password: ${redisPassword}`);
 
 const client = await createClient({url: `${redisUrl}`})
+
 const connect = async () => {
   try {
     await client.connect();
@@ -17,13 +18,16 @@ const connect = async () => {
     const value = await client.get('foo');
     console.log(value)
     await client.disconnect();
+    return value
   } catch (error) {
     console.error("Failed to connect to Redis", error);
     console.warn("continuing without Redis");
   }
 }
 
-connect()
-
+Deno.serve({ port: 8080, hostname: "0.0.0.0" }, async (_req) => {
+  const value = await connect()
+  return new Response(value);
+});
 
 
